1fca8c8d2de71137bdabad0291ee4c5bfcdeceb9
Nicholas Haddou <54536702+nicholashaddou@users.noreply.github.com>
2025-11-10 20:59:31 +0100

    added functionalities to split species and the genomic sequence

--- P1.py
+++ P1.py
@@ -2,8 +2,9 @@ import numpy
 import string
 
 def ParseSeqFile(string):
+
     text_file = open(string, 'r')
-    #print(text_file.read())
+
     lines = text_file.readlines()
     dictionary_of_lines = {}
 
@@ -12,14 +13,18 @@ def ParseSeqFile(string):
             if not (lines[0].startswith('>') or lines[0].startswith('\n')):
                 raise ValueError("malformed input at:", lines)
             else:
-                text = lines[1:]
-                species = text.split()[0]
-                the_rest = text.split()[1:]
-                #print(species)
-                #print(the_rest)
+                if not lines[0].startswith('\n'):
+                    text = lines[1:]
+                    species = text.split()[0]
+                    the_rest = text.split()[1:]
+                    print(species)
+                    #print(the_rest)
+                    dictionary_of_lines[species] = the_rest
 
     except ValueError as ve:
         print(ve)
+
+    print(dictionary_of_lines)
 
 ParseSeqFile("dummy file.txt")
 
