7728a72e698fa1dda2c6510d75b66413dd6ef883
haddonic <haddonic@students.zhaw.ch>
2025-11-10 15:28:53 +0100

    optimized P1 by using try except

--- P1.py
+++ P1.py
@@ -6,14 +6,17 @@ def ParseSeqFile(string):
     #print(text_file.read())
     lines = text_file.readlines()
     list_of_lines = []
-    for lines in lines:
-        if not (lines[0].startswith('>') or lines[0].startswith('\n')):
-            print("malformed input at:", lines)
-        # else:
-        #     print("valid input at:", lines)
-        else:
-            list_of_lines.append(lines.strip())
-    print(list_of_lines)
+
+    try:
+        for lines in lines:
+            if not (lines[0].startswith('>') or lines[0].startswith('\n')):
+                raise ValueError("malformed input at:", lines)
+            else:
+                list_of_lines.append(lines.strip())
+    except ValueError as ve:
+        print(ve)
+
+    #print(list_of_lines)
 
 ParseSeqFile("dummy file.txt")
 
