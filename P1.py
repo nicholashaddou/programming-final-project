cb48009590895318a59242ff9de56712b6f49590
Nicholas Haddou <54536702+nicholashaddou@users.noreply.github.com>
2025-11-10 20:52:12 +0100

    added functionalities to split species and the genomic sequence

--- P1.py
+++ P1.py
@@ -5,18 +5,21 @@ def ParseSeqFile(string):
     text_file = open(string, 'r')
     #print(text_file.read())
     lines = text_file.readlines()
-    list_of_lines = []
+    dictionary_of_lines = {}
 
     try:
         for lines in lines:
             if not (lines[0].startswith('>') or lines[0].startswith('\n')):
                 raise ValueError("malformed input at:", lines)
             else:
-                list_of_lines.append(lines.strip())
+                text = lines[1:]
+                species = text.split()[0]
+                the_rest = text.split()[1:]
+                #print(species)
+                #print(the_rest)
+
     except ValueError as ve:
         print(ve)
-
-    #print(list_of_lines)
 
 ParseSeqFile("dummy file.txt")
 
