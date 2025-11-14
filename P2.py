ef4fe3f041b7e5460e6b3343125152ee40606208
haddonic <haddonic@students.zhaw.ch>
2025-11-14 09:49:44 +0100

    Added AlignByDP method to P2

--- P2.py
+++ P2.py
@@ -1,4 +1,11 @@
 import P1
 
+list_of_sequences = P1.dictionary_to_list("dummy file.txt")
+
+def AlignByDP(list_of_sequences):
+    dictionary_of_aligned_sequences = {}
+
+    return dictionary_of_aligned_sequences
 
 
+AlignByDP(list_of_sequences)
