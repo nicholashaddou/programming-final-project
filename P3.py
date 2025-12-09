091f95db2895ce159063d7449293a3a1eaaa7d03
haddonic <haddonic@students.zhaw.ch>
2025-12-09 08:46:58 +0100

    now computes P properly

--- P3.py
+++ P3.py
@@ -1,33 +1,42 @@
-from sympy import sequence
-
 import P2
-
-def get_sequences():
-    pass
+import math
 
 def ComputeDistMatrix():
 
     dictionary_of_distances = P2.AlignByDP()
-    sequences_to_compare = []
 
-    for (key, value), (seq1, seq2) in dictionary_of_distances.items():
+    for key, (seq1, seq2) in dictionary_of_distances.items():
         sequence1 = seq1
         sequence2 = seq2
-        sequences_to_compare.append((sequence1, sequence2))
-        p_value = 0
-        count = 0
+        difference_count = 0
+        length_without_gap = len(sequence1)
 
-        #only works with smaller first sequence
-        for i in sequence1:
-            for char in i:
-                if char == '-':
-                    count += 1
-                    length_without_gap = len(sequence1) - count
-                    if length_without_gap != 0:
-                        p_value = count / length_without_gap
-                    else:
-                        p_value = 30
-        print(count)
+        new_sequence = ""
+        new_sequence2 = ""
+
+        for i, j in zip(seq1, seq2):
+            if i != "-" and j != "-":
+                new_sequence += i
+                new_sequence2 += j
+
+        #this is stupid, need to get rid of
+        for i in range(len(sequence1)):
+            if sequence1[i] != sequence2[i] and (sequence1[i] == '-' or sequence2[i] == '-'):
+                length_without_gap -= 1
+
+        for i in range(len(new_sequence)):
+            if new_sequence[i] != new_sequence2[i]:
+                difference_count += 1
+
+        print(length_without_gap) #correct
+        #print(difference_count)
+        p_value = difference_count / length_without_gap
         print(p_value)
+
+        if p_value >= 0.75:
+            evolutionary_distance = 30
+        else:
+            evolutionary_distance= -(3/4) * math.log(1 - (4/3) * p_value)
+        print(evolutionary_distance)
 
 ComputeDistMatrix()
